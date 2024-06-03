47b3533cbb2802e97b2af7b275f3445e65455849
Mauricio Míguez Núñez <mauricioeme46@outlook.com>
2024-06-03 20:56:16 +0200

    Update Login

---
+++ GestionLogin(ValidacionUsuariosSistema)/app/src/main/java/com/example/loginvalidacionusuarios/Model/Usuarios.kt
@@ -0,0 +1,3 @@
+package com.example.loginvalidacionusuarios.Model
+
+data class Usuarios(var nombre: String, var contrasena: String)
---
+++ GestionLogin(ValidacionUsuariosSistema)/app/src/main/java/com/example/loginvalidacionusuarios/Screen/LoginAppScreen.kt
@@ -0,0 +1,14 @@
+package com.example.loginvalidacionusuarios.Screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun AppScreenLogin() {
+    Box(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
